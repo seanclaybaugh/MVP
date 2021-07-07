53edf76ac41655af3dad4d17cb0efb619f7661ac
seanclaybaugh <sdclaybaugh@gmail.com>
2021-07-07 13:06:15 -0700

    1st stop

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image } from 'react-native';
 
-export default function App() {
+
+
+ function App() {
+  const [User, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.title}>
+        <Text> Welcome to Name Tracker 3000 </Text>
+      </View>
+        <Text> Please login:</Text>
+        <TextInput></TextInput>
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    color: 'white',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  title: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    flex: .25,
+    fontSize: 100,
+  }
 });
+
+
+export default App;
